ced299a2d1a11fbd23c85725c7297fdbab6a48e8
srkarun <arunsrk1223@gmail.com>
2018-12-29 15:39:09 -0600

    added txt file with commands

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ console.log("gitinit - this initializes the directory");
 console.log("gitstatus - this gives status of the git");
 console.log("gitadd - git add filename will add file to git");
 console.log("gitadd - git add . - this stages all files");
+console.log("gitadd - git add .html - this stages all html files and nothing else");
+console.log("gitadd - git add -A adds all files and directories including hidden files");
 console.log("gitcommit - git commit -m "presenttesnse" will commit the file");
 console.log("gitlog - gives logs of gitcommits");
 console.log("gitcheckout - gitcheckout and version name from above log checks you to older version");
+console.log("gitcheckout master - this gets us back to master");
+console.log("git revert --no commit 2ver4r45...HEAD");
+
