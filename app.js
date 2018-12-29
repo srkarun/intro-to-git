00eba494455afcd68b05aabefa358da36fe7d7a7
srkarun <arunsrk1223@gmail.com>
2018-12-29 12:14:07 -0600

    add app.js file

---
+++ app.js
@@ -0,0 +1,4 @@
+console.log("This is colt steel intro to git below are set of gitcommands");
+console.log("gitinit - this initializes the directory");
+console.log("gitstatus - this gives status of the git");
+console.log("gitadd - git add filename will add file to git");
