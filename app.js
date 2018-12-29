099f3f6daedafb6b144c3bc3a1a1dcf8549dd401
srkarun <arunsrk1223@gmail.com>
2018-12-29 12:21:53 -0600

    new line added

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ console.log("This is colt steel intro to git below are set of gitcommands");
 console.log("gitinit - this initializes the directory");
 console.log("gitstatus - this gives status of the git");
 console.log("gitadd - git add filename will add file to git");
+console.log("gitadd - git add . - this stages all files");
 console.log("gitcommit - git commit -m "presenttesnse" will commit the file");
