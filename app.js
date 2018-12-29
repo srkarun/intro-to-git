7a93b4166cccc4141305456f1c4bf0849e74fc00
srkarun <arunsrk1223@gmail.com>
2018-12-29 12:18:11 -0600

    made modifications

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("This is colt steel intro to git below are set of gitcommands");
 console.log("gitinit - this initializes the directory");
 console.log("gitstatus - this gives status of the git");
 console.log("gitadd - git add filename will add file to git");
+console.log("gitcommit - git commit -m "presenttesnse" will commit the file");
